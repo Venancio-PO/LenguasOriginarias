b4d7bedf99b269ead99a2a1c630cbccc8413c71f
Venancio-PO <venancioperez0518@gmail.com>
2026-09-05 14:40:03 -0600

    se trabjó en historia

--- scripts/scriptContent.js
+++ scripts/scriptContent.js
@@ -1,3 +1,8 @@
 document.getElementById('btnAtras').addEventListener('click', function() {
     window.location.href = 'index.html';
 });
+
+document.getElementById('btnhistoria').addEventListener('click', function() {
+    window.location.href = 'contenido/historia.html'; // revisamos la ruta abajo
+});
+        
